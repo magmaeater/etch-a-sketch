ed217a7ca5b197fd1d0dc3d9854aa77367728cea
magma <tanayachyute@gmail.com>
2023-06-25 16:28:29 +0530

    add buttons with styling for cls and random_mode

--- eas.js
+++ eas.js
@@ -2,13 +2,16 @@ let container = document.querySelector(".container");
 let slider = document.getElementById('myRange');
 let val = document.getElementById('value');
 let b = document.getElementById("new_size");
+let cls = document.getElementById("cls");
+let rand = document.getElementById("random_mode");
 let grid_size = slider.value*slider.value;
+let random_mode = false;
 
-//create grid according to slider input
+//create grid according to slider input and clear the screen when cls is pressed
 
 val.innerHTML = slider.value;
 
-create_grid(grid_size);
+create_grid(grid_size, random_mode);
 
 slider.oninput = function() {
     val.innerHTML = this.value;
@@ -18,9 +21,33 @@ slider.oninput = function() {
 
 b.addEventListener('click', () => {
     
-    create_grid(grid_size);
+    create_grid(grid_size, random_mode);
 });
 
+cls.addEventListener('click', () => {
+    for(let p = 1; p <= grid_size; p++)
+    {
+        let clear_block  = document.getElementById(`${p+1}`);
+        clear_block.style.backgroundColor = 'white';
+    }
+})
+
+//random mode implementation where a random color gets drawn 
+
+rand.addEventListener('click', () => {
+
+    if(rand.textContent === 'Active!')
+    {
+        rand.textContent = 'Random Mode!';
+        random_mode = false;
+    }
+    else
+    {
+        rand.textContent = 'Active!';
+        random_mode = true;
+    }
+    create_grid(grid_size, random_mode);
+});
 
 //setup so drawing only occurs when mouse is being held or clicked
 
@@ -37,7 +64,7 @@ container.onmouseup = () => {
 };
 
 //create grid and take input function
-function create_grid(grid_size)
+function create_grid(grid_size, random_mode)
 {
 
     container.replaceChildren();
@@ -53,12 +80,28 @@ function create_grid(grid_size)
         block.id = `${i+1}`;
 
         block.addEventListener('mousedown', () => {
-            block.style.backgroundColor = "black";
+            if(random_mode === false)
+            {
+                block.style.backgroundColor = "black";
+            }
+            else
+            {
+                block.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+            }
+
         });
         block.addEventListener('mouseover', () => {
             if(mousedown === true)
-            {    
-            block.style.backgroundColor = "black";
+            {
+                if(random_mode === false)
+                {        
+                    block.style.backgroundColor = "black";
+                }
+                else
+                {
+                    block.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+                }
+
             }
         });
 
